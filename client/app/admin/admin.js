96936876ce130f9af86e41c9c07cf12ebde5cb75
sanandrady <luvmuthi17>
2015-10-13 01:52:16 +0100

    First commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('pateRefAppApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
